7fd0e85c8e171ff2641e63cecaea765977d60ad4
Yuheng Chang <jonathanyuheng@gmail.com>
2018-12-02 00:03:16 -0600

    add current arduino code and update readme

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# Self-Driving-Car
+# Self Driven Car
+
+A self driven car with intelligence detecting obstacles and make correct decision based on machine learning.
+
+## Getting Started
+
+This is a hardware based repository. Code platform is Arduino IDE and several hardware pieces assembled in correct manners.
+
+### Prerequisites
+
+Hardware:
+```
+9V Battery 
+Arduino UNO
+H-Bridge (SM754410)
+Ultrasonic Sensor (HC-SR04)
+Breadboard - Self-Adhesive
+DC Barrel Male Jack : 2.1x5.5mm or DC Barrel Female Plug : 2.1x5.5mm
+9V Battery Clip w/ 4" Leads
+```
+Software:
+
+```
+Arduino IDE https://www.arduino.cc/en/Main/Software
+Good Text Editor :)
+```
+
+### Installing
+
+I will make a video on detailed intallation method.
+
+## Running the tests
+
+I will make a video on expected behavior of the motor.
+
+## Built With
+
+<!-- * [Dropwizard](http://www.dropwizard.io/1.0.2/docs/) - The web framework used
+* [Maven](https://maven.apache.org/) - Dependency Management
+* [ROME](https://rometools.github.io/rome/) - Used to generate RSS Feeds -->
+Library needed.
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Authors
+
+* **Yuheng Chang**
+
---
+++ self-drive-car.ino
@@ -0,0 +1,95 @@
+const int MotorLeftDirection2A = 2;
+const int MotorLeftDirection1A = 4;
+const int MotorLeftSpeed = 6;
+
+const int MotorRightDirection4A = 12;
+const int MotorRightDirection3A = 7;
+const int MotorRightSpeed = 9;
+
+const int triggerPin = 10;
+const int echoPin = 11;
+const int ultrasonicSource = 8;
+
+long duration;
+int distance;
+unsigned long trackTime;
+
+void setup() {
+    pinMode(MotorLeftDirection1A, OUTPUT);
+    pinMode(MotorLeftDirection2A, OUTPUT);
+    pinMode(MotorLeftSpeed, OUTPUT);
+
+    pinMode(MotorRightDirection4A, OUTPUT);
+    pinMode(MotorRightDirection3A, OUTPUT);
+    pinMode(MotorRightSpeed, OUTPUT);
+
+    pinMode(triggerPin, OUTPUT); // Sets the trigPin as an Output
+    pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+    pinMode(ultrasonicSource, OUTPUT);
+    Serial.begin(9600); // Starts the serial communication
+
+    duration = 0;
+    distance = 0;
+    trackTime = 0;
+}
+
+void runForward() {
+    digitalWrite(MotorLeftDirection1A, HIGH);
+    digitalWrite(MotorLeftDirection2A, LOW);
+    analogWrite(MotorLeftSpeed, 145);
+
+    digitalWrite(MotorRightDirection4A, HIGH);
+    digitalWrite(MotorRightDirection3A, LOW);
+    analogWrite(MotorRightSpeed, 125);
+}
+
+void runBackward() {
+    digitalWrite(MotorLeftDirection1A, LOW);
+    digitalWrite(MotorLeftDirection2A, HIGH);
+    analogWrite(MotorLeftSpeed, 145);
+
+    digitalWrite(MotorRightDirection4A, LOW);
+    digitalWrite(MotorRightDirection3A, HIGH);
+    analogWrite(MotorRightSpeed, 125);
+}
+
+void turnRight() {
+    digitalWrite(MotorLeftDirection1A, HIGH);
+    digitalWrite(MotorLeftDirection2A, LOW);
+    analogWrite(MotorLeftSpeed, 140);
+
+    digitalWrite(MotorRightDirection4A, LOW);
+    digitalWrite(MotorRightDirection3A, HIGH);
+    analogWrite(MotorRightSpeed, 125);
+}
+
+void loop() {
+    trackTime = millis();
+    digitalWrite(ultrasonicSource, HIGH);
+    runForward();
+    //runBackward();
+    //turnRight();
+
+    digitalWrite(triggerPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(triggerPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(triggerPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distance = duration * 0.034 / 2;
+
+//    Serial.print("Distance: ");
+//    Serial.println(distance);
+
+    if (distance < 4 && trackTime >= 1000) {
+        // change the direction to backward.
+        runBackward();
+        delay(400);
+        // only turn the left wheel forward to turn right.
+        turnRight();
+        delay(365);
+        // turn all the wheel forward.
+        runForward();
+    }
+
+}
